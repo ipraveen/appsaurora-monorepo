a8052a832789e240ff1d90738dd370249c49cad6
Praveen Singh <praveensingh.name@gmail.com>
2023-06-08 18:01:23 +0530

    push

--- packages/ui-components/webpack.config.js
+++ packages/ui-components/webpack.config.js
@@ -25,19 +25,20 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
             {
-                test: /\.(png|jp(e*)g|svg)$/,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 8000, // Convert images < 8kb to base64 strings
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            name: 'images/[hash]-[name].[ext]',
+                        },
+                    },
+                ],
+            },
         ],
     },
     externals: {
         react: 'react',
-        '@emotion/styled': '@emotion/styled',
     },
 };
